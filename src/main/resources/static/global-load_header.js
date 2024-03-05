0d6939aeb0b8e310ed58b4705780974602dfa186
memurro000 <telenkovvsevolod@gmail.com>
2024-03-05 19:49:56 +0300

    mobile support

--- src/main/resources/static/global-load_header.js
+++ src/main/resources/static/global-load_header.js
@@ -1,3 +1,14 @@
-$(function(){
-    $("#header").load("/header.html"); 
-});
+const SCREEN_WIDTH = window.innerWidth;
+
+if(SCREEN_WIDTH < 1200) {
+    $(function(){
+        $("#header").load("/header-mobile.html"); 
+    });
+} 
+else {
+    $(function(){
+        $("#header").load("/header.html"); 
+    });
+}
+
+
